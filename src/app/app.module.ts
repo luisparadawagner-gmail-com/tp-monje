10a7ba18900ad4860180df759f285e7ef987c4f1
Nicolas <niquitoalexis@gmail.com>
2020-04-16 19:58:38 -0300

    creacion formulario persona

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+// import { PruebaComponent } from './prueba/prueba.component';
+// import { AltaAlumnoComponent } from './alta-alumno/alta-alumno.component';
+import { PersonaComponent } from './formulario/persona/persona.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonaComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
